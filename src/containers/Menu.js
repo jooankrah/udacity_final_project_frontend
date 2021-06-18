947cff672504e162b000867102a3ed6af2d3cce9
jonathan_nsiah <jooankrah@gmail.com>
2021-06-18 17:07:25 +0000

    no message

--- src/containers/Menu.js
+++ src/containers/Menu.js
@@ -37,6 +37,9 @@ export default function AppMenu() {
           onClick={(e) => setselectedMenu(e.key)}
         >
           <Menu.Item key="1">
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item key="2">
             <Link to="/">All campgrounds</Link>
           </Menu.Item>
           <Menu.Item key="3">
