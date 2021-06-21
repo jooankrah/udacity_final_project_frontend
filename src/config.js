e8de194117987c67e3e2eed5b52b718bd9b66fd9
jonathan_nsiah <jooankrah@gmail.com>
2021-06-21 10:50:08 +0000

    no message

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const SERVER_URL =
-  "af8f68e0b84e345249d8592dddcbe0ff-882614750.us-east-2.elb.amazonaws.com";
+  "http://af8f68e0b84e345249d8592dddcbe0ff-882614750.us-east-2.elb.amazonaws.com";
